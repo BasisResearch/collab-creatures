6370e5ce8405a7f21001f773263e321c29015aa1
Marjorie Xie <marjoriexie@gmail.com>
2023-03-21 10:04:13 -0400

    added dynamics to food reward

--- multiagent_foraging_mvp.py
+++ multiagent_foraging_mvp.py
@@ -22,12 +22,13 @@ figures.setup_fig()
 plt.close('all')
 plot_initial_world = False
 plot_T = False
+plotValueFuncAtTime = False
 
 starttime = time.time()
 
 # ************** CREATE ENVIRONMENT ***************************
 
-edge_size = 30 # grid world has dimensions edge_size x edge_size
+edge_size = 20 # grid world has dimensions edge_size x edge_size
 x_arr, y_arr, loc_id_arr = util.create_2Dgrid(edge_size)
 
 # --------------- Build transition matrix-----------------------------
@@ -60,12 +61,18 @@ if plot_T:
 # 1) N_states x 1 one-hot vector indicating locations containing rewards
 # 2) N_states x 1 vector with magnitude of reward at each state/locations
 # 3) array or list of loc_ids (locations) containing rewards 
+N_timesteps = 50
 
 N_food = 2
-phi_food = np.zeros([N_states, 1]) 
+f_food = np.zeros([N_states, 1]) 
+food_trajectory = np.zeros([N_states, N_timesteps])
 list_food_loc_id = np.random.permutation(np.arange(N_states))[:N_food] # randomly choose K locations to place new food 
-phi_food[list_food_loc_id] = 1 
-food_2d_locs = np.reshape(phi_food, [edge_size,edge_size])
+f_food[list_food_loc_id] = 1 # put one food item in the respective locations
+food_init_loc_2d = np.reshape(f_food, [edge_size,edge_size])
+
+#food dynamics
+food_depletion_rate = 0.5 
+epoch_dur = 10 # add new food in random locations every epoch_dur time steps
 
 N_predators = 4
 phi_predator = np.zeros([N_states, 1]) 
@@ -75,8 +82,8 @@ predator_2d_locs = np.reshape(phi_predator, [edge_size,edge_size])
 
 if plot_initial_world:
     fig_env, ax_env = plt.subplots()
-    plt.imshow(food_2d_locs)
-    plt.imshow(food_2d_locs + predator_2d_locs)
+    plt.imshow(food_init_loc_2d)
+    # plt.imshow(food_2d_locs + predator_2d_locs)
 
 # food_loc_id = loc_id_arr[np.random.randint(edge_size**2)] # pick a random loc_id
 # food_loc_id = edge_size**2 - 1 #put the food as far out as possible
@@ -88,8 +95,7 @@ if plot_initial_world:
 
 
 # ----------------------- Add agents -----------------------------------
-N_agents = 9
-N_timesteps = 50
+N_agents = 4
 list_agents = []
 arr_loc_id_allagents = np.zeros(N_agents, dtype=int) # array containing location of each agent (index is agent ID)
 phi_agents = np.zeros([N_states, 1]) # # one-hot vector indicating which locations are occupied by agents (index is loc ID)
@@ -110,43 +116,63 @@ for i in range(N_agents):
 
 for t in range(N_timesteps-1):
     ## ---------------------Update environment----------------------------
-    # features = phi_food# (N_features, N_states) matrix, each row being 
+    # features = f_food# (N_features, N_states) matrix, each row being 
+    
+    #Update food feature vector 
+    # food occupied by an agent decays over time 
+    f_food = f_food - food_depletion_rate * f_food * phi_agents 
+     
+    # randomly add a new food patch every several time steps  
+    if t % epoch_dur == 0:
+        list_newfood_loc_id = np.random.permutation(np.arange(N_states))[:N_food]
+        f_food[list_newfood_loc_id] = 1
+        
+    # save food trajectory for plotting
+    food_trajectory[:, t+1] = f_food.flatten()    # (N_states, N_timesteps)   # save as a sparse matrix?
     
     ## ---------------------Update agents ---------------------------------
     
     for i, agent in enumerate(list_agents):
         # sum_weighted_features = agent.c.T @ features 
         prev_state = int(agent.state_trajectory[t])
-        c_food = 1
-        c_predator = -2
-        c_neighbors = -1
+        c_food = 2
+        c_predator = 0
+        c_otheragents = 0.5
         
         
         phi_agents[prev_state] = 0 # move out of previous location
         agent.phi_neighbors = phi_agents  # assume this agent knows the locations of all other agents
         
-        # sum_weighted_features = c_food * phi_food + c_neighbors * agent.phi_neighbors
+        # sum_weighted_features = c_food * f_food + c_otheragents * agent.phi_neighbors
         
         # compute weighted sum of features
         xloc_allagents, yloc_allagents = util.loc1Dto2D(arr_loc_id_allagents, edge_size)
         xloc_self, yloc_self = util.loc1Dto2D(prev_state, edge_size)
-        social_reward_map = agent.compute_reward_otheragents(xloc_allagents, yloc_allagents, xloc_self, yloc_self, edge_size)
-        social_reward_arr = np.reshape(social_reward_map.T, (N_states, 1))
-        sum_weighted_features = c_food * phi_food  + c_predator * phi_predator #+ c_neighbors * social_reward_arr 
+        f_otheragents_2d = agent.compute_reward_otheragents(xloc_allagents, yloc_allagents, xloc_self, yloc_self, edge_size)
+        f_otheragents_1d = np.reshape(f_otheragents_2d, (N_states, 1))
+        sum_weighted_features = c_food * f_food   + c_otheragents * f_otheragents_1d
+        # sum_weighted_features = c_food * f_food  + c_predator * phi_predator #+ c_otheragents * f_otheragents_1d
         
         # compute value function
         value = agent.SR @ sum_weighted_features         # (N_states, 1) vector
         
         # select next action using the value and eligible states 
-        eligible_states_id = np.nonzero(T_eligible[:, prev_state])[0]       # state IDs of eligible states
-        value_eligible = value[eligible_states_id]
-        next_state = eligible_states_id[np.argmax(value_eligible)]
+        # eligible states are those specified by the transition matrix and states not occupied by other agents
+        eligible_states_id = np.nonzero(T_eligible[:, prev_state] * np.logical_not(phi_agents.flatten()))[0]       # state IDs of eligible states
+        value_eligible = value[eligible_states_id].flatten()   # value of eligible states plus some noise 
+        # value_eligible += 0.01 * np.random.randn(value_eligible.shape[0]) # add some noise 
         
-        # # This policy only works if the value vector is non-negative 
-        # next_state = np.argmax(value * T_eligible[:, prev_state, np.newaxis]) # TO DO: change this to sampling probabilistically 
+        # value_eligible = np.ones(value_eligible.shape[0]) + 0.01 * np.random.randn(value_eligible.shape[0])
+        next_state = eligible_states_id[np.argmax(value_eligible)]  # DETERMINISTIC POLICY that works
+        
+        # #sample eligible states from a categorical distribution whose shape is based on the values 
+        # # convert values into probabilities
+        # prob_arr = value_eligible - np.min(value_eligible) # shift values so they are all positive and add some noise
+        # prob_arr = prob_arr / np.sum(prob_arr) # normalize so they sum to 1
+        # next_state = np.random.choice(eligible_states_id, p=prob_arr)
         
         agent.state_trajectory[t+1] = next_state        # scalar 
-        agent.value_trajectory[:, t+1] = value.flatten()          # (N_states, 1) vector
+        agent.value_trajectory[:, t+1] = value.flatten()          # (N_states, N_timesteps)   
         
         phi_agents[next_state] = 1                     # move into new location 
         arr_loc_id_allagents[i] = next_state
@@ -157,38 +183,38 @@ for i, agent in enumerate(list_agents):
 print('simulation run time = ' + str(endtime - starttime))
 
 #%% plot social reward map for one agent
-# fig, ax = plt.subplots()
-# social_reward_map = agent.social_reward_given_location(arr_loc_id_allagents, edge_size)
-# sns.heatmap(ax=ax, data=social_reward_map, center=0, cbar=True)
-# ax.set_title('social reward map')
+fig, ax = plt.subplots()
+sns.heatmap(ax=ax, data=f_otheragents_2d, center=0, cbar=True)
+ax.set_title('social reward map')
 
 #%%  #------- plot value function for all agents at a specific time point -------
 
-t = 1 # which timestep to look at
-
-ncols = 3
-nrows = int(np.ceil(N_agents / ncols))
-grid_kws = {'wspace': 0.5}
-fig, ax = plt.subplots(nrows, ncols, gridspec_kw=grid_kws)
-plt.suptitle('Values at time t = ' + str(t))
-for  i, agent in enumerate(list_agents):
-    r = int(np.ceil((i+1) / ncols) - 1)
-    c = i % ncols
-    value_heatmap = np.reshape(list_agents[i].value_trajectory[:, t], [edge_size,edge_size]) 
-    sns.heatmap(ax=ax[r,c], data=value_heatmap,  center=0, vmin=-1, vmax=1, square=True)
-    ax[r,c].set_title('value for agent ' + str(i))
+if plotValueFuncAtTime: 
+    t = 1 # which timestep to look at
+    
+    ncols = 3
+    nrows = int(np.ceil(N_agents / ncols))
+    grid_kws = {'wspace': 0.5}
+    fig, ax = plt.subplots(nrows, ncols, gridspec_kw=grid_kws)
+    plt.suptitle('Values at time t = ' + str(t))
+    for  i, agent in enumerate(list_agents):
+        r = int(np.ceil((i+1) / ncols) - 1)
+        c = i % ncols
+        value_heatmap = np.reshape(list_agents[i].value_trajectory[:, t], [edge_size,edge_size]) 
+        sns.heatmap(ax=ax[r,c], data=value_heatmap,  center=0, vmin=-1, vmax=1, square=True)
+        ax[r,c].set_title('value for agent ' + str(i))
 
 
 #%%
 # ************************* CREATE USER INTERFACE ***************************
 
-fig_ui, (ax_main, ax_value) = plt.subplots(1, 2, figsize=(16, 6))
+fig_ui, (ax_main, ax_value) = plt.subplots(1, 2, figsize=(10, 4))
 
 ax_main.set_title('Environment')
-# ax_main.imshow(food_2d_locs)
-sns.heatmap(ax=ax_main, data=food_2d_locs, vmin=-1, vmax=1, center=0, square=True, cbar=False)
+# f_food_2d = np.reshape(f_food, [edge_size,edge_size])
+sns.heatmap(ax=ax_main, data=food_init_loc_2d, vmin=-1, vmax=1, center=0, square=True, cbar=False)
 
-# TO DO: pick one agent to highlight in a different color. Plot in an adjacent 
+# Choose one agent to highlight in magenta. Plot in an adjacent 
 # panel the value function of this agent evolving over time 
 z = 0 # index of highlighted agent
 value_heatmap = np.reshape(list_agents[z].value_trajectory[:, 0], [edge_size,edge_size]) 
@@ -204,18 +230,24 @@ for  i, agent in enumerate(list_agents):
         marker = 'mo'
     else:
         marker = markers[0]
-    one_plot, = ax_main.plot([], [], marker)   
+    one_plot, = ax_main.plot([], [], marker, markersize=5)   
     list_plot_agents.append(one_plot)
 
 
-def update_plot(frame, list_agents, edge_size): #, list_food_loc_id, edge_size):
+def update_plot(frame, list_agents, food_trajectory, edge_size): #, list_food_loc_id, edge_size):
+    fig_ui.suptitle('frame ' + str(frame) + ' / ' + str(N_timesteps))
+    # food locations and quantities
+    food_heatmap = np.reshape(food_trajectory[:, frame], [edge_size,edge_size]) 
+    sns.heatmap(ax=ax_main, data=food_heatmap, vmin=-1, vmax=1, center=0, square=True, cbar=False) #, cbar_ax=ax_cbar, cbar_kws={'shrink':0.5})
+    
+    # x_food, y_food = util.loc1Dto2D(list_food_loc_id, edge_size)
+    # plot_food.set_data(x_food, y_food)
+    
+    # state of each agent 
     for  i, agent in enumerate(list_agents):
         state = int(agent.state_trajectory[frame])
         x, y = util.loc1Dto2D(state, edge_size)
         list_plot_agents[i].set_data(x, y)
-        
-    # x_food, y_food = util.loc1Dto2D(list_food_loc_id, edge_size)
-    # plot_food.set_data(x_food, y_food)
     
     # value function of one agent 
     ax_value.cla()
@@ -224,16 +256,21 @@ def update_plot(frame, list_agents, edge_size): #, list_food_loc_id, edge_size):
     
     return list_plot_agents #, plot_food
 
+fig_ui.tight_layout()
+
 ani = animation.FuncAnimation(fig_ui, update_plot, frames=N_timesteps,
-                              fargs=(list_agents, edge_size)) #, list_food_loc_id, edge_size))
+                              fargs=(list_agents, food_trajectory, edge_size)) #, list_food_loc_id, edge_size))
 
-# saving to m4 using ffmpeg writer
-filename = r"C:\Users\admin\Dropbox\Code\Basis-code\multiagent_foodonly_v2.gif"
-filename = r"C:\Users\admin\Dropbox\Code\Basis-code\multiagent_food_and_otheragents_jump.gif"
-
-ani.save(filename, dpi=300, writer=animation.PillowWriter(fps=2))
-
-# ## saving as an mp4
-filename = r"C:\Users\admin\Dropbox\Code\Basis-code\multiagent_mp4.mp4"
-# ani.save(filename, writer=animation.FFMpegWriter(fps=30) )
+#%%
+saveMovie = False
+if saveMovie:
+    # saving to m4 using ffmpeg writer
+    filename = r"C:\Users\admin\Dropbox\Code\Basis-code\multiagent_foodonly_v2.gif"
+    filename = r"C:\Users\admin\Dropbox\Code\Basis-code\multiagent_lazy_avoid_others.gif"
+    
+    ani.save(filename, dpi=300, writer=animation.PillowWriter(fps=2))
+    
+    # ## saving as an mp4
+    filename = r"C:\Users\admin\Dropbox\Code\Basis-code\multiagent_mp4.mp4"
+    # ani.save(filename, writer=animation.FFMpegWriter(fps=30) )
  

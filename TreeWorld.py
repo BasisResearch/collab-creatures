496d581d3d98c00ebff85cd0cbe8d49c7027e393
Marjorie Xie <marjoriexie@gmail.com>
2023-03-21 10:03:41 -0400

    work in progress setting up multispecies

--- TreeWorld.py
+++ TreeWorld.py
@@ -6,10 +6,42 @@ Created on Mon Mar 13 14:52:17 2023
 """
 
 import numpy as np
+import gridworld_utils as util
 
 class TreeEnvironment(object):
     def __init__(self): 
         return
+    
+class BCCH_species(object):
+    def __init__(self, T_prob, N_states, N_timesteps, discount_factor): 
+        self.discount_factor = discount_factor
+        self.SR = np.linalg.pinv(np.eye(N_states) - discount_factor * T_prob)
+        self.state_trajectory = np.zeros([N_timesteps])
+        self.value_trajectory = np.zeros([N_states, N_timesteps])
+        self.phi_neighbors = np.zeros([N_states, 1]) # in which locations do I see a neighboring agent?
+        return 
+    
+    # def reward_function(self, features):
+    #     # compute weighted sum of features
+    #     xloc_allagents, yloc_allagents = util.loc1Dto2D(arr_loc_id_allagents, edge_size)
+    #     xloc_self, yloc_self = util.loc1Dto2D(prev_state, edge_size)
+    #     f_otheragents_2d = agent.compute_reward_otheragents(xloc_allagents, yloc_allagents, xloc_self, yloc_self, edge_size)
+    #     f_otheragents_1d = np.reshape(f_otheragents_2d, (N_states, 1))
+    #     sum_weighted_features = c_food * phi_food   + c_otheragents * f_otheragents_1d
+    #     return sum_weighted_features
+
+
+class TUTI_species(object):
+    def __init__(self, T_prob, N_states, N_timesteps, discount_factor): 
+        self.discount_factor = discount_factor
+        self.SR = np.linalg.pinv(np.eye(N_states) - discount_factor * T_prob)
+        self.state_trajectory = np.zeros([N_timesteps])
+        self.value_trajectory = np.zeros([N_states, N_timesteps])
+        self.phi_neighbors = np.zeros([N_states, 1]) # in which locations do I see a neighboring agent?
+        return 
+    
+
+
 
 class SimpleAgent(object): 
     def __init__(self, T_prob, N_states, N_timesteps, discount_factor): 
@@ -49,12 +81,18 @@ class SimpleAgent(object):
         N_neighbors = len(xloc_neighbors)  # number of neighboring agents
         
         for k in range(N_neighbors): 
-            x = xloc_neighbors[k] 
-            y = yloc_neighbors[k]
+            col = xloc_neighbors[k] 
+            row = yloc_neighbors[k]
             # create a donut-shaped reward field centered on the neighbor
-            reward_map[x-1 : x+2 , y-1 : y+2] = 1  
-            reward_map[x + 0, y + 0] = -1    # give the neighbor some personal space
+
+            reward_map[row-1 : row+2 , col-1 : col+2] = 0.3
+            reward_map[row + 0, col + 0] = -2   # give the neighbor some personal space
             
+        
+        # make your current state neutral
+        reward_map[yloc_self-1 : yloc_self+2 , xloc_self-1 : xloc_self+2] = 0
+        reward_map[yloc_self, xloc_self] = 0 
+        
         return reward_map
         
         # *********** Alternative strategies ****************************

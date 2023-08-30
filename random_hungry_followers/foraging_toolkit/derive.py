2fd73cdf14dbb5eafe8792d84f2286a92fc81c4f
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-08-30 12:25:48 +0200

    debugging communicates

--- random_hungry_followers/foraging_toolkit/derive.py
+++ random_hungry_followers/foraging_toolkit/derive.py
@@ -30,19 +30,24 @@ def derive_predictors(
     info_time_decay=3,
     info_spatial_decay=0.15,
     finders_tolerance=2,
+    time_shift=0,
 ):
     tr = ft.rewards_to_trace(
         sim.rewards,
         sim.grid_size,
         sim.num_frames,
         rewards_decay,
+        time_shift=time_shift,
     )
     sim.traces = tr["traces"]
     sim.tracesDF = tr["tracesDF"]
     derivation_logger.info("traces done")
 
     vis = ft.construct_visibility(
-        sim.birds, sim.grid_size, visibility_range=visibility_range
+        sim.birds,
+        sim.grid_size,
+        visibility_range=visibility_range,
+        time_shift=time_shift,
     )
     sim.visibility_range = visibility_range
     sim.visibility = vis["visibility"]
@@ -56,6 +61,7 @@ def derive_predictors(
         getting_worse=getting_worse,
         optimal=optimal,
         proximity_decay=proximity_decay,
+        time_shift=time_shift,
     )
     sim.getting_worse = getting_worse
     sim.optimal = optimal
@@ -82,12 +88,33 @@ def derive_predictors(
             info_time_decay,
             info_spatial_decay,
             finders_tolerance=finders_tolerance,
+            time_shift=time_shift,
         )
         sim.communicates = com["communicates"]
         sim.communicatesDF = com["communicatesDF"]
 
-        sim.derivedDF = sim.derivedDF.merge(sim.communicatesDF, how="inner")
+        sim.derivedDF = sim.derivedDF.merge(sim.communicatesDF, how="left")
+
+        sim.derivedDF["communicate"].fillna(0, inplace=True)
+
+        mean_communicate = sim.communicatesDF["communicate"].mean()
+        std_communicate = sim.communicatesDF["communicate"].std()
+
+        sim.derivedDF["communicate_standardized"] = (
+            sim.derivedDF["communicate"] - sim.derivedDF["communicate"].mean()
+        ) / sim.derivedDF["communicate"].std()
+
         derivation_logger.info("communicates done")
+
+    pd.set_option("mode.chained_assignment", None)
+    sim.rewardsDF.loc[:, "time"] = sim.rewardsDF["time"] - time_shift
+    sim.birdsDF.loc[:, "time"] = sim.birdsDF["time"] - time_shift
+    sim.tracesDF.loc[:, "time"] = sim.tracesDF["time"] - time_shift
+    sim.visibilityDF.loc[:, "time"] = sim.visibilityDF["time"] - time_shift
+    sim.proximityDF.loc[:, "time"] = sim.proximityDF["time"] - time_shift
+    sim.how_farDF.loc[:, "time"] = sim.how_farDF["time"] - time_shift
+    sim.communicatesDF.loc[:, "time"] = sim.communicatesDF["time"] - time_shift
+    sim.derivedDF.loc[:, "time"] = sim.derivedDF["time"] - time_shift
 
     # TODO: maybe ignore outliers resulting from bird crowding?
     # sim.derivedDF = sim.derivedDF[sim.derivedDF["proximity"] > 0]

22bfe1092c15bba507548b19bf2bdf0e9f54945b
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-09-06 12:07:46 +0200

    prepping locust for review

--- random_hungry_followers/foraging_toolkit/animate_birds.py
+++ random_hungry_followers/foraging_toolkit/animate_birds.py
@@ -3,6 +3,7 @@ import plotly.graph_objects as go
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 
 def animate_birds(
@@ -54,9 +55,7 @@ def animate_birds(
         com["bird"] = "communicate"
         com = com.reset_index(drop=True)
         df = df.reset_index(drop=True)
-        df = pd.concat(
-            [com, df], axis=0, ignore_index=True, verify_integrity=True
-        )
+        df = pd.concat([com, df], axis=0, ignore_index=True, verify_integrity=True)
 
     fig = px.scatter(df, x="x", y="y", animation_frame="time", color="bird")
 
@@ -134,9 +133,7 @@ def animate_birds(
                     trace.marker.symbol = "circle"
                     trace.marker.color = "orange"
                     trace.showlegend = False
-                    trace.marker.size = (
-                        selected_rows["trace"] * trace_multiplier
-                    )
+                    trace.marker.size = selected_rows["trace"] * trace_multiplier
                     trace.marker.opacity = 0.3
 
     if plot_visibility > 0:
@@ -182,12 +179,16 @@ def animate_birds(
     fig.show()
 
 
-def visualise_bird_predictors(tr, prox, hf, com=None,
-                              vis_sampling_rate = 1):
-    
+def visualise_bird_predictors(tr, prox, hf, com=None, vis_sampling_rate=1):
     def sample_tensor(tensor, vis_sampling_rate):
         sample_size = int(vis_sampling_rate * len(tensor))
         return np.random.choice(tensor, size=sample_size, replace=False)
+
+    def custom_copy(tr):
+        if isinstance(tr, torch.Tensor):
+            return tr.clone()
+        else:
+            return tr.copy()
 
     if vis_sampling_rate != 1:
         tr_sub = sample_tensor(tr, vis_sampling_rate)
@@ -196,11 +197,10 @@ def visualise_bird_predictors(tr, prox, hf, com=None,
         if com is not None:
             com_sub = sample_tensor(com, vis_sampling_rate)
     else:
-        tr_sub = tr.copy()
-        prox_sub = prox.copy()
-        hf_sub = hf.copy()
-        com_sub = com.copy() 
-
+        tr_sub = custom_copy(tr)
+        prox_sub = custom_copy(prox)
+        hf_sub = custom_copy(hf)
+        com_sub = custom_copy(com)
 
     if com is not None:
         df = pd.DataFrame(
@@ -256,7 +256,7 @@ def visualise_bird_predictors(tr, prox, hf, com=None,
     if com is not None:
         fig3 = px.scatter(
             df,
-            title = "Communication",
+            title="Communication",
             x="communicate",
             y="how_far_score",
             opacity=0.3,

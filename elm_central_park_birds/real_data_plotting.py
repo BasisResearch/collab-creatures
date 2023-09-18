d13e434440adc05b8f86d6a247337477f6136227
Emily Mackevicius <emily@Emilys-MacBook-Pro.local>
2023-09-18 04:29:25 -0400

    fixing plots, focusing on cleaned data

--- elm_central_park_birds/real_data_plotting.py
+++ elm_central_park_birds/real_data_plotting.py
@@ -1,102 +1,76 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import seaborn as sns
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import seaborn as sns
-from matplotlib.collections import LineCollection
 
-def generate_trajectory_video(dataframe, output_filename='bird_trajectories.mp4', fps=30, n_frames=None):
-    sorted_data = dataframe.sort_values(by='time')
+def generate_bird_trajectories_video(data, output_path, fps=30, frames=None, max_history=100):
+    display('hello')
+    # Sorting data
+    sorted_data = data.sort_values(by="time")
+    
+    # Getting unique bird IDs
     unique_birds = sorted_data['bird'].unique()
+
+    # Colors for birds
     colors = sns.color_palette("dark", len(unique_birds))
-    
-    # Create initial plot frame
-    fig, ax = plt.subplots()
-    fig.set_facecolor('darkgray')  # Dark gray figure background
-    
-    line_collections = []
-    for idx, bird_id in enumerate(unique_birds):
-        bird_data = sorted_data[sorted_data['bird'] == bird_id]
-        lc = LineCollection([], colors=colors[idx], linewidths=3, alpha=0.8)
-        line_collections.append(lc)
-        ax.add_collection(lc)
 
-    def update(num):
+    # Determine frames to consider
+    if frames is None:
+        frames = len(sorted_data['time'].unique())
+    else:
+        frames = min(frames, len(sorted_data['time'].unique()))
+
+    # Create figure and axis
+    fig, ax = plt.subplots(figsize=(8, 8))
+
+    # Set up formatting for the movie files
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=fps, metadata=dict(artist='Me'), bitrate=1800)
+
+    # Warm background color
+    background_color = '#FAEBD7'
+    fig.patch.set_facecolor(background_color)
+
+    # Set limits and aspect ratio
+    ax.set_xlim(0, 350)
+    ax.set_ylim(250, 0)
+    ax.set_aspect('equal', adjustable='box')
+    ax.axis('off')
+
+    def animate(frame):
+        # Start frame
+        start_frame = max(0, frame - max_history)
+        
+        current_data = sorted_data[(sorted_data['time'] <= frame) & (sorted_data['time'] >= start_frame)]
+        
+        
+        # Clear all scatter plots and lines
+        for coll in ax.collections:
+            coll.remove()
+            
+        for line in ax.lines:
+            line.remove()
+            
         for idx, bird_id in enumerate(unique_birds):
-            bird_data = sorted_data[sorted_data['bird'] == bird_id]
-            if n_frames:
-                bird_data = bird_data.iloc[:min(n_frames, num+1)]
-            else:
-                bird_data = bird_data.iloc[:num+1]
-            
-            x_vals = bird_data['x'].values
-            y_vals = bird_data['y'].values
-            segments = [((x_vals[i-1], y_vals[i-1]), (x_vals[i], y_vals[i])) for i in range(1, len(x_vals))]
-            line_collections[idx].set_segments(segments)
-            line_collections[idx].set_linewidth([3*(i/len(x_vals)) for i in range(len(segments))])
+            bird_data = current_data[current_data['bird'] == bird_id]
+            if len(bird_data) == 0:
+                continue
 
-    ax.set_xlim([0, 350])
-    ax.set_ylim([250, 0])  # Flipped y-axis
-    ax.axis('off')  # Remove the axes
-    ax.set_facecolor('darkgray')  # Dark gray background
-    ax.set_aspect('equal', 'box')  # Consistent aspect ratio
+            # Compute sizes based on how old the point is
+            sizes = np.linspace(1, 10, len(bird_data)) ** 2.5
+            # Create new scatter for this bird
+            ax.scatter(bird_data['x'], bird_data['y'], color=colors[idx], s=sizes, alpha=0.25, edgecolor='none')
+            # Plot lines
+            ax.plot(bird_data['x'], bird_data['y'], color=colors[idx], alpha=0.25, linewidth=1)
 
-    ani_frames = len(sorted_data['time'].unique()) if n_frames is None else n_frames
-    ani = animation.FuncAnimation(fig, update, frames=ani_frames, blit=False, interval=1000/fps, repeat=False)
+
+
+
+                
     
-    # Save to .mp4
-    ani.save(output_filename, writer="ffmpeg", fps=fps)
+    ani = animation.FuncAnimation(fig, animate, frames=frames, repeat=False)
 
-    plt.close(fig)  # Close the figure after saving
+    ani.save(output_path, writer=writer, dpi=300)
 
-# Example usage:
-# generate_trajectory_video(df, 'bird_trajectories.mp4', fps=30)
-
-
-
-# def generate_trajectory_video(dataframe, output_filename='bird_trajectories.mp4', fps=30, n_frames=None):
-#     def update(num, data, ax):
-#         ax.clear()
-#         unique_birds = data['bird'].unique()
-#         colors = sns.color_palette("dark", len(unique_birds))
-        
-#         for idx, bird_id in enumerate(unique_birds):
-#             bird_data = data[data['bird'] == bird_id]
-            
-#             if n_frames:
-#                 bird_data = bird_data.iloc[:min(n_frames, num+1)]
-#             else:
-#                 bird_data = bird_data.iloc[:num+1]
-            
-#             x_vals = bird_data['x'].values
-#             y_vals = bird_data['y'].values
-            
-#             for i in range(1, len(x_vals)):
-#                 ax.plot(x_vals[i-1:i+1], y_vals[i-1:i+1], color=colors[idx], alpha=0.8, linewidth=3*(i/len(x_vals)))
-        
-#         ax.set_xlim([0, 350])
-#         ax.set_ylim([250, 0])  # Flipped y-axis
-#         ax.axis('off')  # Remove the axes
-#         ax.set_facecolor('darkgray')  # Dark gray background
-#         ax.set_aspect('equal', 'box')  # Consistent aspect ratio
-
-#     # Create initial plot frame
-#     fig, ax = plt.subplots()
-#     fig.set_facecolor('darkgray')  # Dark gray figure background
-    
-#     sorted_data = dataframe.sort_values(by='time')
-
-#     ani_frames = len(sorted_data['time'].unique()) if n_frames is None else n_frames
-
-#     ani = animation.FuncAnimation(fig, update, frames=ani_frames, 
-#                                   fargs=[sorted_data, ax], blit=False, interval=1000/fps, repeat=False)
-    
-#     # Save to .mp4
-#     ani.save(output_filename, writer="ffmpeg", fps=fps)
-
-#     plt.close(fig)  # Close the figure after saving
+    plt.close(fig)

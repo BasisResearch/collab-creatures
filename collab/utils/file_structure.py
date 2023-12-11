83b02c7c1b6155c146f45175a50a7fad64c1b2ef
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-12-11 16:20:14 +0100

    communicators simulations with notebook test

---
+++ collab/utils/file_structure.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+def find_repo_root() -> Path:
+    return Path(__file__).parent.parent.parent
--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -2,6 +2,6 @@
 
 
 #TODO modify when notebooks land
-#INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb docs/source/backdoor.ipynb docs/source/dr_learner.ipynb docs/source/mediation.ipynb docs/source/sdid.ipynb docs/source/slc.ipynb"
+INCLUDED_NOTEBOOKS="docs/foraging/communicators/communicators_simulations.ipynb"
 
-#CI=1 pytest --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS
+CI=1 pytest --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS

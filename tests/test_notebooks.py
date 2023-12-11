e1b5972cc6c1aa3d4d2c8626acf1f1441335488e
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-12-11 16:48:24 +0100

    reinstated local notebook test

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,19 +1,19 @@
-# import glob
-# import os
-# import subprocess
+import glob
+import os
+import subprocess
 
-# from collab.utils import find_repo_root
+from collab.utils import find_repo_root
 
-# root = find_repo_root()
+root = find_repo_root()
 
-# os.environ["CI"] = "1"  # possibly redundant
+os.environ["CI"] = "1"  # possibly redundant
 
 
-# def test_notebooks_communicators():
-#     notebook_path = f"{root}/docs/foraging/communicators"
-#     notebooks = glob.glob(os.path.join(notebook_path, "*.ipynb"))
+def test_notebooks_communicators():
+    notebook_path = f"{root}/docs/foraging/communicators"
+    notebooks = glob.glob(os.path.join(notebook_path, "*.ipynb"))
 
-#     # run this command from terminal if the test fails to identify the source of trouble, if any
-#     pytest_command = f"C1=1 python -m pytest --nbval-lax --dist loadscope -n auto {' '.join(notebooks)}"
+    # run this command from terminal if the test fails to identify the source of trouble, if any
+    pytest_command = f"C1=1 python -m pytest --nbval-lax --dist loadscope -n auto {' '.join(notebooks)}"
 
-#     subprocess.run(pytest_command, shell=True, check=True)
+    subprocess.run(pytest_command, shell=True, check=True)

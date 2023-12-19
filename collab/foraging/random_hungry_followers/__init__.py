12403bd73db714e7515642471a54d14da525b52e
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-12-19 06:07:57 +0100

    rhf notebook complete

--- collab/foraging/random_hungry_followers/__init__.py
+++ collab/foraging/random_hungry_followers/__init__.py
@@ -1,2 +1,3 @@
 from .hungry_birds import add_hungry_foragers  # noqa F401
 from .random_foragers import Foragers, RandomForagers  # noqa F401
+from .follower_birds import add_follower_foragers  # noqa F401

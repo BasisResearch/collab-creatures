0ca7c1311310cd4b59f2ba8fba8e0b4e116bcdd5
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-05-27 11:26:10 -0400

    adding velocites wip

--- collab/foraging/toolkit/velocity.py
+++ collab/foraging/toolkit/velocity.py
@@ -3,6 +3,8 @@ from typing import List
 import pandas as pd
 from collab.foraging.toolkit.visibility import filter_by_visibility
 from collab.foraging.toolkit.utils import generate_grid
+from collab.foraging.toolkit.trace import rewards_trace
+import numpy as np
 
 
 def add_velocities_to_foragers(foragers: List[pd.DataFrame]) -> None:
@@ -19,8 +21,8 @@ def add_velocities_to_data_object(data_object) -> None:
 
 def generate_velocity_scores(
     sim,
-    info_time_decay=3,
-    info_spatial_decay=0.15,
+    velocity_time_decay=3,
+    velocity_spatial_decay=0.15,
     finders_tolerance=2,
     time_shift=0,
     grid=None,
@@ -37,7 +39,7 @@ def generate_velocity_scores(
             subject=subject,
             time_shift=time_shift,
             visibility_restriction=visibility_restriction,
-            info_time_decay=info_time_decay,
+            info_time_decay=velocity_time_decay,
             finders_tolerance=finders_tolerance,
             filter_by_on_reward=filter_by_on_reward,
         )
@@ -66,3 +68,31 @@ def generate_velocity_scores(
             print("subject_at_t", subject_at_t)
             print("subject_x_at_t", subject_x_at_t)
             print("subject_y_at_t", subject_y_at_t)
+
+            if slice_t.shape[0] > 0:
+                shifted_velocity_x = list(subject_x_at_t + slice_t['velocity_x'])
+                shifted_velocity_y = list(subject_y_at_t + slice_t['velocity_y'])
+
+                for _step in range(slice_t.shape[0]):
+                    velocity_score["velocity_score"] += rewards_trace(
+                        np.sqrt(
+                            (shifted_velocity_x[_step] - velocity_score["x"]) ** 2
+                            + (shifted_velocity_y[_step] - velocity_score["y"]) ** 2
+                        ),
+                        velocity_spatial_decay,
+                    )
+
+                velocity_score["velocity_score_standardized"] = (
+                    velocity_score["velocity_score"] - velocity_score["velocity_score"].mean()
+                ) / velocity_score["velocity_score"].std()
+
+                velocity_score["time"] = velocity_score["time"]
+
+                velocity_scores_b.append(velocity_score)
+
+            velocity_scores_b_df = pd.concat(velocity_scores_b)
+            velocity_scores.append(velocity_scores_b_df)
+
+    velocity_scoresDF = pd.concat(velocity_scores)
+
+    return {"velocity_scores": velocity_scores, "velocity_scoresDF": velocity_scoresDF}

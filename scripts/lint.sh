d399e0fc98366e48c1b6891a16fdc9546ea01802
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-08-06 17:44:55 -0400

    update scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-mypy --ignore-missing-imports collab/
-isort --check --profile black --diff collab/ tests/
-black --check collab/ tests/
-flake8 collab/ tests/
-black --check docs/
-nbqa autoflake --nbqa-shell --recursive --check docs/
+mypy --ignore-missing-imports collab/ collab2/ tests/
+isort --check --profile="black" --diff collab/ collab2/ tests/
+black --check collab/ collab2/ tests/ docs/
+flake8 collab/ collab2/ tests/
+nbqa --nbqa-shell autoflake --nbqa-shell --recursive --check docs/

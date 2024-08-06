640a3f59b983116803a1fd0a75ed8927c53415dc
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-08-06 17:15:24 -0400

    format

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 isort --profile="black" collab/ collab2/ tests/
 black collab/ collab2/ tests/ docs/
 autoflake --remove-all-unused-imports --in-place --recursive ./collab ./collab2 ./tests
-nbqa --nbqa-shell isort docs/
+nbqa --nbqa-shell isort --profile="black" docs/
 nbqa --nbqa-shell autoflake  --nbqa-shell --remove-all-unused-imports --recursive --in-place docs/  
 

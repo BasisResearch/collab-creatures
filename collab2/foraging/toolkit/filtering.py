911b76cdb604a53cb485797d1017aeb7f26c7e99
Palka Puri <ppuri.cash@gmail.com>
2024-08-06 19:26:03 -0400

    lint

--- collab2/foraging/toolkit/filtering.py
+++ collab2/foraging/toolkit/filtering.py
@@ -1,20 +1,22 @@
-from typing import Any, Callable, List
+from typing import Callable, List, Optional
 
 import numpy as np
 import pandas as pd
 
 
-##PP_comment : even with passing constraint params as a dictionary, we might have a limitation : we cannot have a constraint that depends on quantities derived within the predictor function (eg, fastest v)
-# One workaround could be to locally (i.e. within the function) modify foragersDF with the derived quantities and pass that to the constraint function via filter_by_distance
+# PP_comment : even with passing constraint params as a dictionary, we might have a limitation : 
+# we cannot have a constraint that depends on quantities derived within the predictor function (eg, fastest v)
+# One workaround could be to locally (i.e. within the function) modify foragersDF with the derived quantities 
+# and pass that to the constraint function via filter_by_distance
 def filter_by_distance(
     foragersDF: pd.DataFrame,
     f: int,
     t: int,
     interaction_length: float,
-    interaction_constraint: Callable[
-        [List[int], int, int, pd.DataFrame, Any], List[int]
+    interaction_constraint: Optional[
+        Callable[[List[int], int, int, pd.DataFrame, Optional[dict]], List[int]]
     ] = None,
-    interaction_constraint_params: dict = None,
+    interaction_constraint_params: Optional[dict] = None,
 ) -> List[int]:
     positions = foragersDF[foragersDF["time"] == t].copy()
     positions["distance"] = np.sqrt(
@@ -28,6 +30,6 @@ def filter_by_distance(
 
     if interaction_constraint is not None:
         foragers_ind = interaction_constraint(
-            foragers_ind, f, t, foragersDF, **interaction_constraint_params
+            foragers_ind, f, t, foragersDF, interaction_constraint_params
         )
     return foragers_ind

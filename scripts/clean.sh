ef23fe05799fecc68be2527d33dd8802da2f39e3
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-08-06 17:18:42 -0400

    pull scripts with black in isort profile

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,8 +2,7 @@
 set -euxo pipefail
 
 isort --profile="black" collab/ collab2/ tests/
-black collab/ collab2/ tests/ docs/
 autoflake --remove-all-unused-imports --in-place --recursive ./collab ./collab2 ./tests
 nbqa --nbqa-shell isort --profile="black" docs/
 nbqa --nbqa-shell autoflake  --nbqa-shell --remove-all-unused-imports --recursive --in-place docs/  
-
+black collab/ collab2/ tests/ docs/

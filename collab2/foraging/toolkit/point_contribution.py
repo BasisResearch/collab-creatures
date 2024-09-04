51cd0a6e88501a532bf1d6f715a067d318a4a826
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-09-04 11:56:27 -0400

    add trace predictor (#113)
    * trace code in place
    
    * format lint
    
    * refactored proximity
    
    * removed old proximity code
    
    * format lint
    
    * refactored trace
    
    * added generate food to init
    
    * format, lint
    
    * update parameter names in docstring
    
    * renaming function args
    
    ---------
    
    Co-authored-by: Palka Puri <ppuri.cash@gmail.com>

---
+++ collab2/foraging/toolkit/point_contribution.py
@@ -0,0 +1,27 @@
+from typing import Callable, Union
+
+import numpy as np
+import pandas as pd
+
+from collab2.foraging.toolkit.utils import dataObject  # noqa: F401
+
+
+def _exponential_decay(distance: Union[float, np.ndarray], decay_factor: float = 0.5):
+    return np.exp(-decay_factor * distance)
+
+
+def _point_contribution(
+    x_source: int,
+    y_source: int,
+    grid: pd.DataFrame,
+    contribution_function: Callable = _exponential_decay,
+    **contribution_function_kwargs,
+) -> np.ndarray:
+
+    distance_to_source = np.sqrt(
+        (grid["x"] - x_source) ** 2 + (grid["y"] - y_source) ** 2
+    )
+    contribution = contribution_function(
+        distance_to_source, **contribution_function_kwargs
+    )
+    return contribution

5e07e39e8b6e77b8749ca6062314b3fbf555da12
Palka Puri <ppuri.cash@gmail.com>
2024-09-06 17:39:53 -0400

    simulations + animations

--- collab2/foraging/fish/__init__.py
+++ collab2/foraging/fish/__init__.py
@@ -1,1 +1,2 @@
-from .fish_simulations import Fish_IndependentRates  # noqa: F401
+from .simulation_models import Fish_IndependentRates  # noqa: F401
+from .simulation_viz import animate_trajectories

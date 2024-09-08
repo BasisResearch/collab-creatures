2f79e3635603d8579bfe440e8b8fc8e579293b9f
Palka Puri <ppuri.cash@gmail.com>
2024-09-08 14:57:04 -0400

    refactoring

--- collab2/foraging/fish/simulation_models.py
+++ collab2/foraging/fish/simulation_models.py
@@ -11,9 +11,7 @@ class Fish_IndependentRates:
         Tmax: Optional[float] = 10,
         dt: Optional[float] = 0.1,
         random_state: Optional[int] = 0,
-        vicsek_params: Optional[dict[str, Any]] = None,
-        pairwiseCopying_params: Optional[dict[str, Any]] = None,
-        diffusion_params: Optional[dict[str, Any]] = None,
+        interaction_params: Optional[dict[str, dict[str, Any]]] = None,
     ):
         """
         :param N: number of fish
@@ -21,18 +19,10 @@ class Fish_IndependentRates:
         :param Tmax: simulation end time
         :param dt: time-step for logging fish positions, speed, and heading
         :param random_state: random seed for reproducibility
-        :param vicsek_params: parameters of vicsek interactions in the simulations
-            Must include keys: "rate", "interaction_length", "sigma_v", "sigma_t".
-            Each value must be a list of length = N, indicating parameter choices for each fish
-            in the simulation.
-        :param pairwiseCopying_param: parameters of pairwise copying interactions in the simulations
-            Must include keys: "rate", "interaction_length", "sigma_v", "sigma_t".
-            Each value must be a list of length = N, indicating parameter choices for each fish
-            in the simulation.
-        :param diffusion_params: parameters for velocity diffusion in the simulations
-            Must include keys: "rate", "sigma_v", "sigma_t".
-            Each value must be a list of length = N, indicating parameter choices for each fish
-            in the simulation.
+        :param interaction_params: nested dictionary of parameters of the interactions in the model.
+            the keys of the dictionary are interaction names, each corresponding value is a dictionary of
+            parameters for the interaction, eg, "rate" (required for all interactions). There must exist a method
+            named "{interaction}_interaction" to implement each interaction.
         """
         self.N = N
         self.arena_size = arena_size
@@ -40,32 +30,27 @@ class Fish_IndependentRates:
         self.dt = dt
 
         # choose default parameters
-        if vicsek_params is None:
-            vicsek_params = {
-                "rate": 0.5*np.ones(N),
-                "interaction_length": arena_size / 3 * np.ones(N),
-                "sigma_v": 5 * np.ones(N),
-                "sigma_t": 1 * np.ones(N),
+        if interaction_params is None:
+            interaction_params = {
+                "vicsek": {
+                    "rate": 0.5 * np.ones(N),
+                    "interaction_length": arena_size / 3 * np.ones(N),
+                    "sigma_v": 5 * np.ones(N),
+                    "sigma_t": 1 * np.ones(N),
+                },
+                "pairwiseCopying": {
+                    "rate": 0.5 * np.ones(N),
+                    "interaction_length": arena_size / 3 * np.ones(N),
+                    "sigma_v": 5 * np.ones(N),
+                    "sigma_t": 1 * np.ones(N),
+                },
+                "diffusion": {
+                    "rate": 0.5 * np.ones(N),
+                    "sigma_v": 5 * np.ones(N),
+                    "sigma_t": 1 * np.ones(N),
+                },
             }
-
-        if pairwiseCopying_params is None:
-            pairwiseCopying_params = {
-                "rate": 0.5*np.ones(N),
-                "interaction_length": arena_size / 3 * np.ones(N),
-                "sigma_v": 5 * np.ones(N),
-                "sigma_t": 1 * np.ones(N),
-            }
-
-        if diffusion_params is None:
-            diffusion_params = {
-                "rate": 0.5*np.ones(N),
-                "sigma_v": 5 * np.ones(N),
-                "sigma_t": 1 * np.ones(N),
-            }
-
-        self.vicsek_params = vicsek_params
-        self.pairwiseCopying_params = pairwiseCopying_params
-        self.diffusion_params = diffusion_params
+        self.interaction_params = interaction_params
 
         ## initialize matrices to save trajectory and velocity information ##
         time_steps = np.ceil(Tmax / dt).astype(int) + 1
@@ -150,120 +135,135 @@ class Fish_IndependentRates:
             # update index
             idx_0 += 1
 
-    def time_to_next_reaction(self):
+    def time_to_next_interaction(self):
         total_rate = np.sum(
-            np.concatenate((
-                self.vicsek_params["rate"],
-                self.pairwiseCopying_params["rate"],
-                self.diffusion_params["rate"]
-                ))
-            )
-        
+            [
+                np.sum(self.interaction_params[key]["rate"])
+                for key in self.interaction_params.keys()
+            ]
+        )
         r1 = self.rng.random()
-        return 1/total_rate * np.log(1/r1)
-    
-    def sample_next_reaction(self):
-        rate_matrix =  np.stack((
-                self.vicsek_params["rate"],
-                self.pairwiseCopying_params["rate"],
-                self.diffusion_params["rate"]
-                ), axis = 1 ) #each fish corresponds to a separate row
-        
+        return 1 / total_rate * np.log(1 / r1)
+
+    def sample_next_interaction(self):
+        rate_matrix = np.stack(
+            (
+                [
+                    self.interaction_params[key]["rate"]
+                    for key in self.interaction_params.keys()
+                ]
+            ),
+            axis=1,
+        )  # each fish corresponds to a separate row
+
         p_fish = np.sum(rate_matrix, axis=1) / np.sum(rate_matrix, axis=None)
 
-        #sample a fish
-        f = self.rng.choice(range(self.N), p = p_fish)
+        # sample a fish
+        f = self.rng.choice(range(self.N), p=p_fish)
 
-        #sample which reaction occurs
-        reactions_list = ["vicsek", "pairwiseCopying", "diffusion"]
-        p_reactions = rate_matrix[f,:] / np.sum(rate_matrix[f,:])
-        reaction = self.rng.choice(reactions_list,p=p_reactions)
-        return f,reaction
+        # sample which reaction occurs
+        interactions_list = list(self.interaction_params.keys())
+        p_interactions = rate_matrix[f, :] / np.sum(rate_matrix[f, :])
+        interaction = self.rng.choice(interactions_list, p=p_interactions)
+        return f, interaction
 
-    def find_neighbors(self,f,t_ind,L):
-        """ 
+    def find_neighbors(self, f, t_ind, L):
+        """
         find neighbors of fish f at time index t_ind given interaction length L
         """
-        dist2 = (self.trajectories[f,t_ind,0] - self.trajectories[:,t_ind,0])**2 + (self.trajectories[f,t_ind,1] - self.trajectories[:,t_ind,1])**2
+        dist2 = (
+            self.trajectories[f, t_ind, 0] - self.trajectories[:, t_ind, 0]
+        ) ** 2 + (self.trajectories[f, t_ind, 1] - self.trajectories[:, t_ind, 1]) ** 2
         dist2[f] = np.nan
         neighbors = np.flatnonzero(dist2 < L**2)
         return neighbors
-    
-    def vicsek_reaction(self,f):
-        t_ind = int(self.time/self.dt)
-        L = self.vicsek_params["interaction_length"][f]
-        sigma_v = self.vicsek_params["sigma_v"][f]
-        sigma_t = self.vicsek_params["sigma_t"][f]
 
-        neighbors = self.find_neighbors(f,t_ind,L)
-        
+    def vicsek_interaction(self, f):
+        t_ind = int(self.time / self.dt)
+        L = self.interaction_params["vicsek"]["interaction_length"][f]
+        sigma_v = self.interaction_params["vicsek"]["sigma_v"][f]
+        sigma_t = self.interaction_params["vicsek"]["sigma_t"][f]
+
+        neighbors = self.find_neighbors(f, t_ind, L)
+
         if len(neighbors):
-            #find average velocity
-            vx = np.mean(self.velocities[neighbors,t_ind,0] * np.cos(self.velocities[neighbors,t_ind,1]))
-            vy = np.mean(self.velocities[neighbors,t_ind,0] * np.sin(self.velocities[neighbors,t_ind,1]))
-            v = np.sqrt(vx**2 + vy**2) 
-            theta = np.arctan2(vy,vx)
+            # find average velocity
+            vx = np.mean(
+                self.velocities[neighbors, t_ind, 0]
+                * np.cos(self.velocities[neighbors, t_ind, 1])
+            )
+            vy = np.mean(
+                self.velocities[neighbors, t_ind, 0]
+                * np.sin(self.velocities[neighbors, t_ind, 1])
+            )
+            v = np.sqrt(vx**2 + vy**2)
+            theta = np.arctan2(vy, vx)
 
             # add noise and update f
-            self.velocities[f,t_ind,0] = v + self.rng.normal(loc=0,scale=sigma_v)
-            self.velocities[f,t_ind,1] = theta + self.rng.normal(loc=0,scale=sigma_t)
+            self.velocities[f, t_ind, 0] = v + self.rng.normal(loc=0, scale=sigma_v)
+            self.velocities[f, t_ind, 1] = theta + self.rng.normal(loc=0, scale=sigma_t)
 
             # make sure velocity magnitude is not negative!
-            self.velocities[f,t_ind,0] = np.maximum(0,self.velocities[f,t_ind,0])
+            self.velocities[f, t_ind, 0] = np.maximum(0, self.velocities[f, t_ind, 0])
 
-    def pairwiseCopying_reaction(self, f):
-        t_ind = int(self.time/self.dt)
-        L = self.pairwiseCopying_params["interaction_length"][f]
-        sigma_v = self.pairwiseCopying_params["sigma_v"][f]
-        sigma_t = self.pairwiseCopying_params["sigma_t"][f]
+    def pairwiseCopying_interaction(self, f):
+        t_ind = int(self.time / self.dt)
+        L = self.interaction_params["pairwiseCopying"]["interaction_length"][f]
+        sigma_v = self.interaction_params["pairwiseCopying"]["sigma_v"][f]
+        sigma_t = self.interaction_params["pairwiseCopying"]["sigma_t"][f]
 
-        neighbors = self.find_neighbors(f,t_ind,L)
-        
+        neighbors = self.find_neighbors(f, t_ind, L)
+
         if len(neighbors):
-            #choose a neighbor to randomly copy
+            # choose a neighbor to randomly copy
             j = self.rng.choice(neighbors)
 
             # add noise and update f
-            self.velocities[f,t_ind,0] = self.velocities[j,t_ind,0] + self.rng.normal(loc=0,scale=sigma_v)
-            self.velocities[f,t_ind,1] = self.velocities[j,t_ind,1] + self.rng.normal(loc=0,scale=sigma_t)
+            self.velocities[f, t_ind, 0] = self.velocities[
+                j, t_ind, 0
+            ] + self.rng.normal(loc=0, scale=sigma_v)
+            self.velocities[f, t_ind, 1] = self.velocities[
+                j, t_ind, 1
+            ] + self.rng.normal(loc=0, scale=sigma_t)
 
             # make sure velocity magnitude is not negative!
-            self.velocities[f,t_ind,0] = np.maximum(0,self.velocities[f,t_ind,0])
+            self.velocities[f, t_ind, 0] = np.maximum(0, self.velocities[f, t_ind, 0])
 
-    def diffusion_reaction(self,f):
-        t_ind = int(self.time/self.dt)
-        sigma_v = self.diffusion_params["sigma_v"][f]
-        sigma_t = self.diffusion_params["sigma_t"][f]
-        # add noise 
-        self.velocities[f,t_ind,0] += self.rng.normal(loc=0,scale=sigma_v)
-        self.velocities[f,t_ind,1] += self.rng.normal(loc=0,scale=sigma_t)
+    def diffusion_interaction(self, f):
+        t_ind = int(self.time / self.dt)
+        sigma_v = self.interaction_params["diffusion"]["sigma_v"][f]
+        sigma_t = self.interaction_params["diffusion"]["sigma_t"][f]
+        # add noise
+        self.velocities[f, t_ind, 0] += self.rng.normal(loc=0, scale=sigma_v)
+        self.velocities[f, t_ind, 1] += self.rng.normal(loc=0, scale=sigma_t)
         # make sure velocity magnitude is not negative!
-        self.velocities[f,t_ind,0] = np.maximum(0,self.velocities[f,t_ind,0])
+        self.velocities[f, t_ind, 0] = np.maximum(0, self.velocities[f, t_ind, 0])
 
-        
     def simulate(self):
         # t_init = 0
         # nSteps = np.ceil(self.Tmax/self.dt).astype(int)
         # self.evolve_positions(t_init, nSteps)
-        
+
         while True:
-            # find time to next reaction 
-            tau = self.time_to_next_reaction()
+            # find time to next reaction
+            tau = self.time_to_next_interaction()
 
             # evolve positions until next reaction
-            idx_0 = int(self.time/self.dt)
-            idx_f = np.minimum(idx_0 + np.round(tau/self.dt), np.ceil(self.Tmax/self.dt)).astype(int)
-            self.evolve_positions(idx_0,idx_f)
+            idx_0 = int(self.time / self.dt)
+            idx_f = np.minimum(
+                idx_0 + np.round(tau / self.dt), np.ceil(self.Tmax / self.dt)
+            ).astype(int)
+            self.evolve_positions(idx_0, idx_f)
 
-            #update time in increments of dt 
+            # update time in increments of dt
             self.time = idx_f * self.dt
 
-            # choose which reaction occurs 
-            f, reaction = self.sample_next_reaction()
+            # choose which reaction occurs
+            f, interaction = self.sample_next_interaction()
 
-            # apply transformation 
-            reaction_func = getattr(self, f"{reaction}_reaction")
-            reaction_func(f)
+            # apply transformation
+            interaction_func = getattr(self, f"{interaction}_interaction")
+            interaction_func(f)
 
             if self.time >= self.Tmax:
                 break
--- collab2/foraging/fish/simulation_viz.py
+++ collab2/foraging/fish/simulation_viz.py
@@ -1,13 +1,14 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import numpy as np
 from IPython.display import HTML
+from matplotlib.animation import FuncAnimation
+
 
 def animate_trajectories(sim, interval=100):
-    
-    x = sim.trajectories[:,:,0] # shape : nfish x time
-    y = sim.trajectories[:,:,1]
-    theta = sim.velocities[:,:,1]
+
+    x = sim.trajectories[:, :, 0]  # shape : nfish x time
+    y = sim.trajectories[:, :, 1]
+    theta = sim.velocities[:, :, 1]
     N = sim.N
     n_frames = x.shape[1]
     arena_size = sim.arena_size
@@ -15,28 +16,30 @@ def animate_trajectories(sim, interval=100):
 
     # Set up the figure and axis
     fig, ax = plt.subplots()
-    ax.set_xlim(-arena_size,arena_size)
-    ax.set_ylim(-arena_size,arena_size)
+    ax.set_xlim(-arena_size, arena_size)
+    ax.set_ylim(-arena_size, arena_size)
     ax.set_aspect("equal")
 
     # Initialize scatter for particle positions and lines for velocity directions
-    particles = ax.scatter(x[:,0], y[:,0], color='blue')
-    velocity_lines = [ax.plot([], [], color='blue')[0] for _ in range(N)]
+    particles = ax.scatter(x[:, 0], y[:, 0], color="blue")
+    velocity_lines = [ax.plot([], [], color="blue")[0] for _ in range(N)]
 
     # plot arena outline
-    temp = np.linspace(0,2*np.pi, 100)
-    ax.plot(arena_size*np.cos(temp), arena_size*np.sin(temp), 'k')
+    temp = np.linspace(0, 2 * np.pi, 100)
+    ax.plot(arena_size * np.cos(temp), arena_size * np.sin(temp), "k")
 
     # Update function for each frame
     def update(frame):
         # Update the particle positions
-        particles.set_offsets(np.c_[x[:,frame], y[:,frame]])
+        particles.set_offsets(np.c_[x[:, frame], y[:, frame]])
 
         # Update velocity direction lines
         for i, line in enumerate(velocity_lines):
-            dx = - 5*np.cos(theta[i,frame])
-            dy = - 5*np.sin(theta[i,frame])
-            line.set_data([x[i,frame], x[i,frame] + dx], [y[i,frame], y[i,frame] + dy])
+            dx = -5 * np.cos(theta[i, frame])
+            dy = -5 * np.sin(theta[i, frame])
+            line.set_data(
+                [x[i, frame], x[i, frame] + dx], [y[i, frame], y[i, frame] + dy]
+            )
             ax.set_title(f"t={frame*dt : .2f}")
         return particles, *velocity_lines
 
@@ -45,4 +48,3 @@ def animate_trajectories(sim, interval=100):
 
     # Display the animation inline in Jupyter Notebook
     return HTML(anim.to_jshtml())
-

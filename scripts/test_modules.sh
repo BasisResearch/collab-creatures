a7c446e9b48681f6ee371d01dae9063e47279977
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-10-16 08:05:21 -0400

    add smoke test loigic to communicators inference

--- scripts/test_modules.sh
+++ scripts/test_modules.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 ./scripts/lint.sh
-pytest -s --cov=collab2/ --cov=tests --cov-report=term-missing ${@-} --cov-report=html:tests/coverage
+python -m pytest -s --cov=collab2/ --cov=tests --cov-report=term-missing ${@-} --cov-report=html:tests/coverage

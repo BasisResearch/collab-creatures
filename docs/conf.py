f37fdb5a7e343545a8dcdfcd994712399e9aea4b
Dmitry Batenkov <dima.batenkov@gmail.com>
2024-10-16 11:09:59 -0400

    Format

--- docs/conf.py
+++ docs/conf.py
@@ -8,36 +8,37 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 import sphinx_rtd_theme
 
-project = 'Collaborative Intelligence Systems'
-copyright = '2024, Basis Research Institute'
-author = 'Basis Research Institute'
-release = '1.0.0'
+project = "Collaborative Intelligence Systems"
+copyright = "2024, Basis Research Institute"
+author = "Basis Research Institute"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.viewcode',
-    'nbsphinx',
-    'sphinxcontrib.bibtex',
-    'sphinxcontrib.jquery',
-    'myst_parser',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
+    "sphinxcontrib.bibtex",
+    "sphinxcontrib.jquery",
+    "myst_parser",
 ]
 
 # Point sphinxcontrib.bibtex to the bibtex file.
-bibtex_bibfiles = ['refs.bib']
+bibtex_bibfiles = ["refs.bib"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-language = 'en'
+language = "en"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -51,5 +52,5 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
+    "python": ("https://docs.python.org/3", None),
 }

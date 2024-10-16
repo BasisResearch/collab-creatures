4495a2cd5191adc24e59fa5ebc5b3a3d7b0664a3
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-10-16 17:07:41 -0400

    withdraw xetcache

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "torch", "scipy", "scikit-learn",
         "matplotlib>=3.8.2", "dill", "torchdiffeq",
         "numpy==2.1.2",
-        "xetcache @ git+https://github.com/xetdata/xetcache.git"
     ],
     extras_require={
         "extras": EXTRAS_REQUIRE,

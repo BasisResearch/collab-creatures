c649f194f51c61ab8b8329ec42b8c38488d8df0f
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-10-16 13:36:09 -0400

    smoke test logic

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -4,6 +4,6 @@
 #TODO modify when notebooks land
 INCLUDED_NOTEBOOKS="docs/foraging/ docs/experimental/collab2_tests"
 
-EXCLUDED_NOTEBOOK="docs/foraging/locust/locust_ds_validate.ipynb docs/experitmental/collab2_tests/vicsek_predictors.ipynb"
-
+EXCLUDED_NOTEBOOK="docs/foraging/locust/locust_ds_validate.ipynb"
+ 
 CI=1 python -m pytest -v --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS --ignore=$EXCLUDED_NOTEBOOK

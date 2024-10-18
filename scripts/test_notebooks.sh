e84e1d2398e511f190a28c6697b0de1d6c82f0a0
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-10-18 09:37:13 -0400

    tests

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -6,4 +6,4 @@ INCLUDED_NOTEBOOKS="docs/foraging/ docs/experimental/collab2_tests"
 
 EXCLUDED_NOTEBOOK="docs/foraging/locust_ds_validate.ipynb"
 
-CI=1 pytest -v --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS --ignore=$EXCLUDED_NOTEBOOK
+CI=1 python -m pytest -v --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS --ignore=$EXCLUDED_NOTEBOOK

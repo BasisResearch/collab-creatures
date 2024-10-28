3f6f1ba26efc6e24c99b54be31954bbf6e275c74
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-10-28 11:40:17 -0400

    lint

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-isort --profile="black" collab/ collab2/ tests/
+isort --profile="black" collab/ tests/
 autoflake --remove-all-unused-imports --in-place --recursive ./collab  ./tests
 nbqa --nbqa-shell isort --profile="black" docs/
 nbqa --nbqa-shell autoflake  --nbqa-shell --remove-all-unused-imports --recursive --in-place docs/  
-black collab/ collab2/ tests/ docs/
+black collab/ tests/ docs/

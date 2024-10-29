a0b4654f152b2e87083357af0b34d0f1c14d3870
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-10-29 17:40:58 -0400

    follower debugging WIP

--- collab/foraging/random_hungry_followers/follower_birds.py
+++ collab/foraging/random_hungry_followers/follower_birds.py
@@ -33,7 +33,6 @@ def add_follower_foragers(
 
     old_foragers = sim.foragers.copy()
 
-    # TODO Check if different forager types mix well
     how_many_foragers_already = len(old_foragers)
 
     new_foragers = sim.generate_random_foragers(
@@ -45,9 +44,13 @@ def add_follower_foragers(
         new_forager["type"] = "follower"
 
     for t in range(0, sim.num_frames):
+
+        sim.foragers.extend(new_foragers)
+
+
         if t > 0 and t % 10 == 0:
             logging.info(f"Generating frame {t}/{sim.num_frames} ")
-        # change to num frames
+
         _vis = construct_visibility(
             new_foragers,
             sim.grid_size,
@@ -56,9 +59,6 @@ def add_follower_foragers(
             end=t + 1,
         )["visibility"]
 
-#        print("in vis:", _vis[0][0]['time'][0])
-        # print first line of visibility
-        # print("vis", 0, _vis[0][0].head(1))
 
         _prox = generate_proximity_score(
             new_foragers,
@@ -67,24 +67,18 @@ def add_follower_foragers(
             getting_worse=getting_worse,
             optimal=optimal,
             proximity_decay=proximity_decay,
-            start=0, #these are relative positions to _vis, which is generated for t only, so 0 in _vis is t
+            start=0, # these are relative positions to _vis, 
+                     # which is generated for t only, so 0 in _vis is t
             end=1,
             time_shift=t,
         )["proximity"]
 
-        # print("prox", _prox[0][0].head(1))
-
-        #assert  _prox[0][0]["time"][0] == _vis[0][0]["time"][0], "time mismatch"
 
         for b in range(num_follower_foragers):
             options = copy.deepcopy(_vis[b][0])
 
             
-            # print("option")
-            # print(options.head(1))
             options = options.merge(_prox[b][0], how="inner")
-            # print("options_merged")
-            # print(options.head(1))
 
             if b == 1:
                 plt.scatter(options["x"], options["y"], c = options["proximity"])
@@ -102,13 +96,10 @@ def add_follower_foragers(
 
             chosen_option = options.iloc[np.random.randint(0, 10)]
 
-            # if b == 1:
-            #     print(chosen_option)
-                # chosen_option = options.head(0)
-
             if t < sim.num_frames - 1:
                 new_x = chosen_option["x"]
                 new_y = chosen_option["y"]
+
 
 
                 new_row = {
@@ -119,14 +110,13 @@ def add_follower_foragers(
                     "type": "follower",
                 }
                
+                new_foragers[b] = pd.DataFrame(new_row, index = [0])
+                #new_foragers[b].loc[len(new_foragers[b])] = new_row
 
-                new_foragers[b].loc[len(new_foragers[b])] = new_row
 
-                if t >= 15 and t <= 20 and b == 1:
-                    print(new_row)
-                   
-    sim.foragers.extend(new_foragers)
     sim.foragersDF = pd.concat(sim.foragers)
+                   
+    
 
     rew = update_rewards(sim, sim.rewards, sim.foragers, start=1)
 
@@ -134,3 +124,39 @@ def add_follower_foragers(
     sim.rewardsDF = rew["rewardsDF"]
 
     return sim
+
+
+
+import collab.foraging.toolkit as ft
+from collab.foraging.random_hungry_followers.random_foragers import Foragers
+import random
+
+num_frames = 5
+
+
+random.seed(23)
+np.random.seed(23)
+
+# create a new empty simulation (a starting point for the actual simulation)
+grid_size = 60
+follower_sim = Foragers(
+    grid_size=grid_size,
+    num_foragers=3,
+    num_frames=num_frames,
+    num_rewards=30,
+    grab_range=3,
+)
+
+# run the simulation: this places the rewards on the grid
+follower_sim()
+
+# add the followers to the simulation and run simulation forward
+follower_sim = add_follower_foragers(
+    follower_sim,
+    num_follower_foragers=3,
+    visibility_range=45,
+    getting_worse=.5,
+    optimal=3,
+    proximity_decay=2,
+    initial_positions=np.array([[10, 10], [20, 20], [40, 40]]),
+)

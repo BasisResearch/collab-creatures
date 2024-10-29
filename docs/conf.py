87860cf0f42c32e557f8a9f19b0671b074160b41
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-10-29 15:03:39 -0400

    add paths to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath('./foraging'))
+sys.path.insert(0, os.path.abspath('./foraging/random-hungry-followers'))
 import sphinx_rtd_theme
 
 project = "Collaborative Intelligence Systems"
